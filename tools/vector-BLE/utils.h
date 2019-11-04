f6e38de891fed8396dbdf434ef8906834e9a1902
Randall Maas <randym@acm.org>
2019-11-03 18:26:58 -0600

    vector-BLE

--- tools/vector-BLE/utils.h
+++ tools/vector-BLE/utils.h
@@ -16,12 +16,56 @@ extern "C"
     void bleSend(void const* bytes, size_t length);
 };
 
-/// This is used to map a 16-bit little endian to the native format
-#define LEU16_decode(p) (((p)[0] & 0xFFuL)| (((p)[1] << 8) & 0xFF00uL))
-#define LEU32_decode(p) (LEU16_decode(p)|((uint32_t)LEU16_decode(p+2)<<16))
-
 /// The version of the protocol and message formats
 extern int protoVersion;
+
+/// A pointer into the payload area of the message
+extern uint8_t* out_msg;
+
+/// This is used to map a 16-bit little endian to the native format
+#define LEU16_decode(p) (((p)[0] & 0xFFuL)| (((p)[1] << 8) & 0xFF00uL))
+
+/// This is used to map a 32-bit little endian to the native format
+#define LEU32_decode(p) (LEU16_decode(p)|((uint32_t)LEU16_decode(p+2)<<16))
+
+
+/** This is used to encode a 16-bit number into the little endian order
+    @param buf  The buffer to populate
+    @param v    The 16-bit value to place into the buffer
+ 
+    Note: this is done by the explicit (tedious) method of populating the
+    buffer instead of casting a pointer (e.g. *(uint16_t)*buf=v;)  That approach
+    bus faults on some architectures if the buffer isn't aligned just right.
+ */
+inline void LEU16_encode(void* buf, uint16_t v)
+{
+    uint8_t* _buf = (uint8_t*) buf;
+    *_buf++ = v&0xff; v>>=8;
+    *_buf++ = v&0xff; v>>=8;
+}
+
+/** This is used to encode a 32-bit number into the little endian order
+    @param buf  The buffer to populate
+    @param v    The 32-bit value to place into the buffer
+ 
+    Note: this is done by the explicit (tedious) method of populating the
+    buffer instead of casting a pointer (e.g. *(uint32_t)*buf=v;)  That approach
+    bus faults on some architectures if the buffer isn't aligned just right.
+ */
+inline void LEU32_encode(void* buf, uint32_t v)
+{
+    uint8_t* _buf = (uint8_t*) buf;
+    *_buf++ = v&0xff; v>>=8;
+    *_buf++ = v&0xff; v>>=8;
+    *_buf++ = v&0xff; v>>=8;
+    *_buf++ = v&0xff; v>>=8;
+}
+
+/** A helper to convert ASCII hex to binary
+    @param c characte to convert
+    @returns the converted value
+ */
+extern uint8_t ascii2hex(char c);
 
 
 /** This is called to interpret message
@@ -41,8 +85,37 @@ extern void CLAD_ready(void);
  */
 extern void CLAD_interpret(uint8_t type, uint8_t const* msg, size_t size, uint8_t version);
 
+/** This is called to request status info from Vector
+ */
+extern void status_req(void);
 
-/// This is used to map a 16-bit little endian to the native format
-#define LEU16_decode(p) (((p)[0] & 0xFFuL)| (((p)[1] << 8) & 0xFF00uL))
+/** This is called to request a log from Vector
+ 
+    The follow up should call log_resp() and fileDownload_recv()
+ */
+extern void log_req(void);
 
+/** This is called to request a cloud session
+    @param token         The session token from the cloud service
+    @param clientName    The name of the computer[?]
+    @param applicationId The name of the application software
+ */
+extern void CloudSession_req(char const* token, char const* clientName, char const* applicationId);
+
+
+/** This is called to request Vector become a WIFI access point
+    @param enable  0 to disable the WIFI access point, otherwuse to enable the
+                access point
+
+    If not cloud authorized, this will give a response 0x21 (response message)
+    with an error message that this is not cloud authorized.
+
+    Under other conditions (TBD) an "access point" response will come back and
+    explain what is going on
+ 
+    The hypothesis is that enabling the WIFI access point will enable a UDP
+    server on Vector that is similar to the one on Cozmo; the pycozmo figured
+    much of Cozmo's protocol.
+ */
+extern void WiFi_AP_req(uint8_t enable);
 #endif /* utils_h */
